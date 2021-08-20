5fc09773e67102705a944154a4a97157cc4492b4
Mateusz Lisowski <lisina091@gmail.com>
2021-08-20 21:02:09 +0200

    2zmiana

--- my-page/src/components/sites/Main.tsx
+++ my-page/src/components/sites/Main.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import bootstrap from 'bootstrap'
+import '../uielements/mypage.css'
+import testowe from '../uielements/testowa.jpg'
+import { tekst, navar } from '../uielements/tekst'
+
+
+
+
 
 const Main = () => {
     return (
-        <div>
+        <div className="container">
+            <body>
+                <div className="MainDiv col-sm-6 col-md-5">
 
+                    <img src={testowe} alt="jbzd"></img>
+                    {tekst}
+                    {navar}
+
+                </div>
+            </body>
         </div>
     )
 }
---
+++ my-page/src/components/uielements/tekst.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const tekst = "czy to jedyna"
+export const navar = 'opcja zeby to dzialalo'
+
+
+
+
