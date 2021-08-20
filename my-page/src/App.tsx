dd8bf1b0aa2f6459b6048c1970279b3d1922bcb9
Mateusz Lisowski <lisina091@gmail.com>
2021-08-20 19:18:31 +0200

    1zmiana

--- my-page/src/App.tsx
+++ my-page/src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Main from './components/sites/Main';
+import { ReactFragment } from 'react';
 
+import './App.css';
+import About from './components/sites/About';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Fragment>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Main />
+          </Route>
+          <Route path="/About">
+            <About />
+          </Route>
+        </Switch>
+      </Router>
+    </Fragment>
+
   );
 }
 
